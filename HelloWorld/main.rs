66db24396b23f4f1501adc7a98fdc0026c47a042
jesseaam <18119820+jesseaam@users.noreply.github.com>
2022-11-05 14:51:53 -0400

    wrote hello world program

---
+++ HelloWorld/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    // println! is NOT a fn, rather a macro. Macros use a bang to distinguish them from normal method calls.
+    // macros are executed at compile time. Can take variable number of arguments.
+    println!("Hello, world!");
+}
