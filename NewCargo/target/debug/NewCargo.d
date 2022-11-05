79c68ddb0fc29835e7ba37feba7ad3f36c51f617
jesseaam <18119820+jesseaam@users.noreply.github.com>
2022-11-05 15:19:17 -0400

    build program with cargo

---
+++ NewCargo/target/debug/NewCargo.d
@@ -0,0 +1,1 @@
+/Users/jmarks/Documents/code/RustProjects/Rust-Essential-Training/NewCargo/target/debug/NewCargo: /Users/jmarks/Documents/code/RustProjects/Rust-Essential-Training/NewCargo/src/main.rs
---
+++ NewCargo/target/debug/deps/NewCargo-e4c180795207f1a3.d
@@ -0,0 +1,5 @@
+/Users/jmarks/Documents/code/RustProjects/Rust-Essential-Training/NewCargo/target/debug/deps/NewCargo-e4c180795207f1a3: src/main.rs
+
+/Users/jmarks/Documents/code/RustProjects/Rust-Essential-Training/NewCargo/target/debug/deps/NewCargo-e4c180795207f1a3.d: src/main.rs
+
+src/main.rs:
